a7f343b8d24ec059eb0e9345aaba24de1db75800
summer <1551282386@qq.com>
2022-04-03 21:43:00 +0800

    修复删除报表文件失败

--- src/main/java/com/ureport/ureportkeep/console/designer/DesignerController.java
+++ src/main/java/com/ureport/ureportkeep/console/designer/DesignerController.java
@@ -241,7 +241,8 @@ public class DesignerController extends AbstractReportBasicController {
     }
 
     @RequestMapping(value = "/deleteReportFile", method = RequestMethod.POST)
-    public void deleteReportFile(HttpServletRequest req) throws ServletException, IOException {
+    @ResponseBody
+    public R deleteReportFile(HttpServletRequest req) throws ServletException, IOException {
         String file = req.getParameter("file");
         if (file == null) {
             throw new ReportDesignException("Report file can not be null.");
@@ -257,6 +258,8 @@ public class DesignerController extends AbstractReportBasicController {
             throw new ReportDesignException("File [" + file + "] not found available report provider.");
         }
         targetReportProvider.deleteReport(file);
+
+        return R.ok();
     }
 
 }
