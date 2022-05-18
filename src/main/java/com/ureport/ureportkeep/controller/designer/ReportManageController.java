e8a0c290ea217f142deef3cdc5633c7974917efe
summer <1551282386@qq.com>
2022-05-18 22:48:46 +0800

    完善打开报表接口、跨域解决

--- src/main/java/com/ureport/ureportkeep/controller/designer/ReportManageController.java
+++ src/main/java/com/ureport/ureportkeep/controller/designer/ReportManageController.java
@@ -4,10 +4,17 @@ import com.ureport.ureportkeep.console.common.R;
 import com.ureport.ureportkeep.controller.designer.dto.ReportManageDto;
 import com.ureport.ureportkeep.core.init.ReportProvidersInit;
 import com.ureport.ureportkeep.core.provider.report.ReportProvider;
+import org.apache.commons.io.IOUtils;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.util.StringUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -21,6 +28,8 @@ public class ReportManageController {
 
     @Autowired
     private ReportProvidersInit reportProvidersInit;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     /**
      * 加载报表列表
@@ -43,11 +52,35 @@ public class ReportManageController {
      * @param file 报表文件名
      * @return
      */
-    @RequestMapping(value = "/loadReport", method = RequestMethod.GET)
-    public R loadReport(@RequestParam String file) {
+    @RequestMapping(value = "/loadReport", method = RequestMethod.POST)
+    @CrossOrigin
+    public String loadReport(@RequestParam String provider, @RequestParam String file) {
+        String sheetsStr = "";
+        List<Map> results = new ArrayList<>();
 
+        ReportProvider reportProvider = reportProvidersInit.getProvider(provider);
+        InputStream inputStream = null;
+        try {
+            inputStream = reportProvider.loadReport(file);
+            byte[] bytes = new byte[10240];
+            IOUtils.read(inputStream, bytes);
 
-        return R.ok();
+            results.add(objectMapper.readValue(bytes, Map.class));
+            sheetsStr = objectMapper.writeValueAsString(results);
+            return sheetsStr;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sheetsStr;
     }
 
     /**
