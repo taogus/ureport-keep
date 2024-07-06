9514a7c4bc0c68ca7055e71d4aff9668e07440fc
march <1551282386@qq.com>
2024-07-06 16:28:28 +0800

    fix: 修复 [formatdate] 函数第二个参数不生效问题

--- src/main/java/com/ureport/ureportkeep/core/expression/function/FormatDateFunction.java
+++ src/main/java/com/ureport/ureportkeep/core/expression/function/FormatDateFunction.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2017 Bstek
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -24,6 +24,7 @@ import com.ureport.ureportkeep.core.expression.model.data.ExpressionData;
 import com.ureport.ureportkeep.core.expression.model.data.ObjectExpressionData;
 import com.ureport.ureportkeep.core.expression.model.data.ObjectListExpressionData;
 import com.ureport.ureportkeep.core.model.Cell;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
@@ -36,51 +37,70 @@ import java.util.List;
  */
 @Component
 public class FormatDateFunction implements Function {
-	private final String defaultPattern="yyyy-MM-dd HH:mm:ss";
-	@Override
-	public Object execute(List<ExpressionData<?>> dataList, Context context, Cell currentCell) {
-		if(dataList==null){
-			return "";
-		}
-		Object obj=null;
-		String pattern=defaultPattern;
-		for(ExpressionData<?> data:dataList){
-			if(data instanceof ObjectListExpressionData){
-				ObjectListExpressionData listExpressionData=(ObjectListExpressionData)data;
-				List<?> list=listExpressionData.getData();
-				if(list.size()>0){
-					obj=list.get(0);
-				}
-				if(list.size()>1){
-					pattern=list.get(1).toString();
-				}
-			}else if(data instanceof ObjectExpressionData){
-				obj=((ObjectExpressionData)data).getData();
-			}else if(data instanceof BindDataListExpressionData){
-				BindDataListExpressionData bindDataList=(BindDataListExpressionData)data;
-				List<BindData> list=bindDataList.getData();
-				if(list.size()>0){
-					obj=list.get(0).getValue();
-				}
-				if(list.size()>1){
-					pattern=list.get(1).getValue().toString();
-				}
-			}
-		}
-		if(obj==null){
-			throw new ReportComputeException("Function [formatdate] need a Date type parameter at least");
-		}else{
-			if(obj instanceof Date){
-				SimpleDateFormat sd=new SimpleDateFormat(pattern);
-				return sd.format((Date)obj);
-			}else{
-				throw new ReportComputeException("Function [formatdate] first parameter is Date type");
-			}
-		}
-	}
+    private final String defaultPattern = "yyyy-MM-dd HH:mm:ss";
 
-	@Override
-	public String name() {
-		return "formatdate";
-	}
+    @Override
+    public Object execute(List<ExpressionData<?>> dataList, Context context, Cell currentCell) {
+        Object dateValue = null;
+        String pattern = defaultPattern;
+
+		int size = dataList.size();
+        if (size == 0) {
+            return "";
+        }
+        ExpressionData<?> expressionData = dataList.get(0);
+        dateValue = parseData(expressionData);
+		if (size > 1) {
+            pattern = (String) parseData(dataList.get(1));
+            if (StringUtils.isEmpty(pattern)) {
+                throw new ReportComputeException("函数 [formatdate] 第二个参数值不能为空");
+            }
+        }
+
+        if (dateValue == null) {
+            throw new ReportComputeException("函数 [formatdate] 参数不能为空");
+        } else {
+            SimpleDateFormat sd = new SimpleDateFormat(pattern);
+            if (dateValue instanceof Number) {
+                long unixTime = ((Number) dateValue).longValue();
+                int numberLength = String.valueOf(unixTime).length();
+                if (numberLength == 10) {
+                    unixTime = unixTime * 1000;
+                }
+                dateValue = new Date(unixTime);
+            }
+
+            if (dateValue instanceof Date) {
+                return sd.format(dateValue);
+            } else {
+                throw new ReportComputeException("函数 [formatdate] 第一个参数不是可解析类型");
+            }
+        }
+    }
+
+    private Object parseData(ExpressionData<?> data) {
+        Object value = null;
+        if (data instanceof ObjectListExpressionData) {
+            ObjectListExpressionData listExpressionData = (ObjectListExpressionData) data;
+            List<?> list = listExpressionData.getData();
+            if (!list.isEmpty()) {
+                value = list.get(0);
+            }
+        } else if (data instanceof ObjectExpressionData) {
+            value = ((ObjectExpressionData) data).getData();
+        } else if (data instanceof BindDataListExpressionData) {
+            BindDataListExpressionData bindDataList = (BindDataListExpressionData) data;
+            List<BindData> list = bindDataList.getData();
+            if (!list.isEmpty()) {
+                value = list.get(0).getValue();
+            }
+        }
+
+        return value;
+    }
+
+    @Override
+    public String name() {
+        return "formatdate";
+    }
 }
